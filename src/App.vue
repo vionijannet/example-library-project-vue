eb2f3ed8b1e6951c348d600849cf6606c4133e71
Vioni <vionijannet@gmail.com>
2024-10-22 14:41:26 +0700

    Init project

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <span class="card-expand">
+    <p class="cutoff-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt enim vitae impedit unde rem consectetur ipsa nihil laudantium, possimus quidem, sint provident dolorem distinctio reprehenderit atque dolores consequatur sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt enim vitae impedit unde rem consectetur ipsa nihil laudantium, possimus quidem, sint provident dolorem distinctio reprehenderit atque dolores consequatur sed.
+    </p>
+    <input type="checkbox" name="" id="" class="expand-btn">
+  </span>
+  <span class="card-expand">
+    <p class="cutoff-text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt enim vitae impedit unde rem consectetur ipsa nihil laudantium, possimus quidem, sint provident dolorem distinctio reprehenderit atque dolores consequatur sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo deserunt enim vitae impedit unde rem consectetur ipsa nihil laudantium, possimus quidem, sint provident dolorem distinctio reprehenderit atque dolores consequatur sed.
+    </p>
+    <input type="checkbox" name="" id="" class="expand-btn">
+  </span>
+  <div>
+    <ButtonGista @click="alertSomething">hehehe</ButtonGista>
+    <InputGista v-model="input" :labelText="'hehehe'"></InputGista>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const input = ref("");
+function alertSomething(): void {
+  window.alert("pe");
+}
+</script>
+
+<style scoped>
+.cutoff-text {
+  --max-lines: 1;
+  --line-height: 1.4;
+
+  max-height: calc(var(--max-lines) * 1em * var(--line-height));
+  line-height: var(--line-height);
+
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: relative;
+}
+
+.cutoff-text::before {
+  content: "";
+  position: absolute;
+  height: calc(1em * var(--line-height));
+  widows: 100%;
+  bottom: 0;
+  pointer-events: none;
+}
+
+.expand-btn {
+  appearance: none;
+  cursor: pointer;
+}
+
+.expand-btn::before {
+  content: "show more";
+}
+
+.expand-btn:checked:before {
+  content: "show less";
+}
+
+.cutoff-text:has(+ .expand-btn:checked) {
+  max-height: none;
+}
+</style>
